6ca1757a18cb898836e160608110e6f54c3f7300
tosirap <parisotpaul@live.fr>
2019-06-25 15:52:52 +0200

    role ok

--- src/components/content/ContentComputer/Computers.vue
+++ src/components/content/ContentComputer/Computers.vue
@@ -1,86 +1,87 @@
 <template>
-  <div>
-    <CustomTableOrdi striped hover :update="this.update" :delete="this.delete" :items="this.computerList" :add="this.add" :companies="this.companyList"></CustomTableOrdi>
-  </div>
+    <div>
+        <CustomTableOrdi striped hover :update="this.update" :delete="this.delete" :items="this.computerList"
+                         :add="this.add" :companies="this.companyList"></CustomTableOrdi>
+    </div>
 </template>
 
 <script>
-import CustomTableOrdi from "./CustomTableOrdi";
-import axios from "axios";
-import { mapGetters } from "vuex";
+    import CustomTableOrdi from "./CustomTableOrdi";
+    import axios from "axios";
+    import {mapGetters} from "vuex";
 
-export default {
-  name: "computers",
-  props: ["updateVar", "componentUpdate"],
-  components: { CustomTableOrdi },
-  data() {
-    return {
-      computerList: [],
-	  errors: [],
-	  companyList: []
+    export default {
+        name: "computers",
+        props: ["updateVar", "componentUpdate"],
+        components: {CustomTableOrdi},
+        data() {
+            return {
+                computerList: [],
+                errors: [],
+                companyList: []
+            };
+        },
+        computed: {
+            ...mapGetters(["page", "size", "search"])
+        },
+        methods: {
+            get() {
+                axios
+                    .get(
+                        "http://10.0.1.97:8080/cdb/api/computers" +
+                        "?page=" + this.page +
+                        "&size=" + this.size +
+                        "&search=" + this.search +
+                        "&orderBy=id"
+                    )
+                    .then(response => (this.computerList = response.data))
+                    .catch(e => {
+                        this.errors.push(e);
+                    });
+            },
+            delete(listId) {
+                listId.forEach(elem => {
+                    axios
+                        .delete("http://10.0.1.97:8080/cdb/api/computers/" + elem)
+                        .then(() => this.get());
+                });
+            },
+            update(computer) {
+                if (computer.introduction === '') computer.introduction = null;
+                if (computer.discontinued === '') computer.discontinued = null;
+                axios
+                    .put("http://10.0.1.97:8080/cdb/api/computers/", computer).then(() => this.get())
+                    .catch(e => {
+                        this.errors.push(e);
+                    });
+            },
+            add(computer) {
+                axios.post('http://10.0.1.97:8080/cdb/api/computers', computer)
+                    .then(() => this.get())
+            },
+            getCompanies() {
+                axios
+                    .get('http://10.0.1.97:8080/cdb/api/companies')
+                    .then(response => {
+                        this.companyList = response.data
+                    })
+            }
+        },
+        created() {
+            this.get();
+            this.getCompanies();
+        },
+
+        watch: {
+            page: function () {
+                this.get();
+            },
+            size: function () {
+                this.get();
+            },
+            search: function () {
+                this.get();
+            }
+        }
     };
-  },
-  computed: {
-    ...mapGetters(["page", "size", "search"])
-  },
-  methods: {
-    get() {
-      axios
-        .get(
-          "http://10.0.1.97:8080/cdb/api/computers"+
-            "?page=" + this.page +
-            "&size=" + this.size +
-            "&search=" + this.search + 
-            "&orderBy=id"
-        )
-        .then(response => (this.computerList = response.data))
-        .catch(e => {
-          this.errors.push(e);
-        });
-    },
-    delete(listId) {
-      listId.forEach(elem => {
-        axios
-          .delete("http://10.0.1.97:8080/cdb/api/computers/" + elem)
-          .then(() => this.get());
-      });
-    },
-      update(computer) {
-          if(computer.introduction==='') computer.introduction=null;
-          if(computer.discontinued==='') computer.discontinued=null;
-          axios
-                  .put("http://10.0.1.97:8080/cdb/api/computers/", computer).then(()=>this.get())
-                  .catch(e => {
-                      this.errors.push(e);
-                  });
-      },
-    add(computer){
-      axios.post('http://10.0.1.97:8080/cdb/api/computers', computer)
-              .then(()=>this.get())
-    },
-    getCompanies(){
-       axios
-        .get('http://10.0.1.97:8080/cdb/api/companies')
-        .then(response => {
-			this.companyList = response.data
-  		})
-    }
-  },
-  created() {
-	this.get();
-	this.getCompanies();
-  },
-
-  watch: {
-    page: function() {
-      this.get();
-    },
-    size: function() {
-      this.get();
-    },
-    search: function() {
-      this.get();
-    }
-  }
-};
 </script>
--- src/components/content/ContentRole/Roles.vue
+++ src/components/content/ContentRole/Roles.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <CustomTableRole striped hover :items="this.roleList"></CustomTableRole>
+        <CustomTableRole striped hover  :update="this.update" :delete="this.delete" :add="this.add" :items="this.roleList"></CustomTableRole>
     </div>
 </template>
 
@@ -10,6 +10,7 @@
     import { mapGetters } from 'vuex';
     export default {
         name: 'roles',
+        props: ["updateVar", "componentUpdate"],
         components: {CustomTableRole},
         data() {
             return {
@@ -20,19 +21,37 @@
         computed: {
             ...mapGetters([
                 'page',
-                'size'
+                'size', "search"
             ])
         },
         methods: {
             get() {
                 axios
                     .get(
-                            "http://10.0.1.97:8080/cdb/api/roles?page="+this.page+"&size="+this.size
+                            "http://10.0.1.97:8080/cdb/api/roles?page="+this.page+"&size="+this.size+"&orderBy=id&search="+this.search
                         )
                         .then(response => (this.roleList = response.data))
                         .catch(e => {
                             this.errors.push(e);
                         });
+            },
+            delete(listId) {
+                listId.forEach(elem => {
+                    axios
+                        .delete("http://10.0.1.97:8080/cdb/api/roles/" + elem)
+                        .then(() => this.get());
+                });
+            },
+            update(role) {
+                axios
+                    .put("http://10.0.1.97:8080/cdb/api/roles/", role)
+                    .catch(e => {
+                        this.errors.push(e);
+                    });
+            },
+            add(role) {
+                axios.post('http://10.0.1.97:8080/cdb/api/roles', role)
+                    .then(()=>this.get())
             }
         },
         created() {
@@ -44,6 +63,9 @@
             },
             size: function() {
                 this.get()
+            },
+            search: function() {
+                this.get();
             }
         }
     }
