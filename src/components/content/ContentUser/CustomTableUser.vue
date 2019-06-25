9ba1e490cb8273264df4c8403464c58145c2351b
jingan35 <jude_ingani@hotmail.com>
2019-06-25 18:07:39 +0200

    mettre en place update

--- src/components/content/ContentUser/CustomTableUser.vue
+++ src/components/content/ContentUser/CustomTableUser.vue
@@ -20,8 +20,42 @@
             </b-col>
         </b-row>
 
+         <b-row>
+            <b-col md="6" class="my-1">
+                <b-input-group>
+                    <b-input-group-append>
+                        <p v-if="!deleteMode">
+                            <b-button class="btn btn-danger" v-on:click="deleteMode=(!deleteMode)">Delete</b-button>
+                            <b-button v-b-modal.modal-2>New User</b-button>
+
+                            <b-modal ref="my-modal" id="modal-2" title="New User">
+                                <AddForm :add="this.add" :hideModal="this.hideModal" :roles="this.roles"></AddForm>
+                                <div slot="modal-footer">
+                                    <b-button slot="modal-cancel" @click="this.hideModal">Cancel</b-button>
+                                </div>
+                            </b-modal>
+
+                        </p>
+                        <p v-else>
+                            <b-button class="btn" v-on:click="deleteMode=(!deleteMode)">Cancel</b-button>
+                        </p>
+                        <div v-if="deleteMode && selectedDelete.length>0 ">
+                            <b-button v-b-modal.modal-1 class="btn btn-danger">Validate</b-button>
+                            <b-modal id="modal-1" title="Are you sure you want to delete ?" hide-footer>
+                                <template slot="modal-title">Are you sure you want to delete ?</template>
+                                <div class="d-block text-center">
+                                    <b-button class="mt-3" block @click="validationSupression">Yes</b-button>
+                                    <b-button class="mt-3" block @click="$bvModal.hide('modal-1')">No</b-button>
+                                </div>
+                            </b-modal>
+                        </div>
+                    </b-input-group-append>
+                </b-input-group>
+            </b-col>
+        </b-row>
+
         <!-- Main table element -->
-        <b-table :items="this.items" :fields="fields">
+        <b-table hover :items="styleDanger" @row-clicked="selectionDelete" :fields="fields">
 
             <template slot="id" slot-scope="row">
                 {{ row.value}}
@@ -44,45 +78,82 @@
                 <b-button size="sm" class="mr-2" v-on:click="updating = ligne.item.id"> Update</b-button>
             </template>
 
-            <template slot="delete">
-                <b-button size="sm" class="mr-2"> Delete</b-button>
-            </template>
+            
 
         </b-table>
 
-
-        <!-- Info modal -->
-        <!-- <b-modal :id="infoModal.id" :title="infoModal.title" ok-only @hide="resetInfoModal">
-             <pre>{{ infoModal.content }}</pre>
-         </b-modal>-->
     </b-container>
 </template>
 
 <script>
     import { mapMutations, mapGetters } from 'vuex';
+    import AddForm from "./FormAddUser";
+
     export default {
         name: "CustomTableUser",
-        props: ['items'],
+        props: ['items','add','delete','roles','update'],
         methods:{
             ...mapMutations([
                 'setSize'
             ]),
             ...mapGetters([
                 'size'
-            ])
+            ]),
+            selectionDelete(render) {
+                if (this.selectedDelete.includes(render.id) && this.deleteMode) {
+                    const index = this.selectedDelete.indexOf(render.id);
+                    this.selectedDelete.splice(index, 1);
+                } else if (this.deleteMode) {
+                    this.selectedDelete.push(render.id);
+                }
+            },
+            validationSupression: function () {
+                this.delete(this.selectedDelete);
+                this.selectedDelete = [];
+                this.deleteMode = false;
+            },
+            hideModal() {
+                this.$refs['my-modal'].hide()
+            }
+        },
+        components:{
+            AddForm
         },
         data(){
             return{
                 filter: null,
                 pageOptions: [10, 50, 100],
-                fields:['id', 'username', 'roleId', 'roleName', 'update', 'delete'],
-                updating: -1,
-                currentSize: this.size()
+                perPage: 10,
+                fields:['id', 'username', 'roleId', 'roleName', 'update'],
+                updating: null,
+                currentSize: this.size(),
+                selectedDelete: [],
+                deleteMode: false
             }
         },
         watch: {
-            currentSize: function(value) {
-                this.setSize(value)
+            currentSize: function (value) {
+                this.setSize(value);
+            },
+            filter: function (value) {
+                this.setSearch(value);
+            },
+            page: function () {
+				this.updating = null;
+				this.selectedDelete = [];
+            }
+        },
+        computed:{
+            styleDanger() {
+                return this.items.map(item => {
+                    if (this.selectedDelete.includes(item.id) && this.deleteMode) {
+                        item._rowVariant = "danger";
+                        return item;
+                    } else {
+                        item._rowVariant = "";
+                        return item;
+                    }
+                });
             }
         }
     }
--- src/components/content/ContentUser/Users.vue
+++ src/components/content/ContentUser/Users.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <CustomTableUser striped hover :items="this.userList"></CustomTableUser>
+        <CustomTableUser striped hover :items="this.userList" :update="this.update" :add="this.add" :delete="this.delete" :roles="this.roleList"></CustomTableUser>
     </div>
 </template>
 
@@ -15,29 +15,55 @@
         data() {
             return {
                 userList: [],
-                errors: []
+                errors: [],
+                roleList: []
             }
         },
         computed: {
             ...mapGetters([
-                'page',
-                'size'
+                'page','size','search'
             ])
         },
         methods:{
             get() {
                 axios
                     .get(
-                            "http://10.0.1.97:8080/cdb/api/users?page="+this.page+"&size="+this.size
+                            "http://10.0.1.97:8080/cdb/api/users?page="+this.page+"&size="+this.size+"&search="+this.search+"&orderBy=id"
                         )
                         .then(response => (this.userList = response.data))
                         .catch(e => {
                             this.errors.push(e);
                         });
+                    },
+            delete(listId) {
+            listId.forEach(elem => {
+                axios
+                .delete("http://10.0.1.97:8080/cdb/api/users/" + elem)
+                .then(() => this.get());
+            });
+            },
+            update(user) {
+                axios
+                        .put("http://10.0.1.97:8080/cdb/api/users/", user).then(()=>this.get())
+                        .catch(e => {
+                            this.errors.push(e);
+                        });
+            },
+            add(user){
+            axios.post('http://10.0.1.97:8080/cdb/api/users', user)
+                    .then(()=>this.get())
+            },
+            getRoles(){
+            axios
+                .get("http://10.0.1.97:8080/cdb/api/roles")
+                .then(response => {
+                    this.roleList = response.data
+                })
             }
         },
         created() {
             this.get()
+            this.getRoles();
         },
         watch: {
             page: function() {
